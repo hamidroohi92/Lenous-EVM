91503ecead9f02bd22e3f1cb575e98931e2c0fd7
Hamid Roohi <hamidroohi71@gmail.com>
2025-03-26 16:18:08 +0330

    update server url

--- app/dataRequests/constants.ts
+++ app/dataRequests/constants.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = "https://lenous-server.liara.run/api/v1";
-// export const SOCKET_URL = "https://lenous-server.liara.run";
-export const BASE_URL = "http://localhost:3000/api/v1";
-export const SOCKET_URL = "http://localhost:3000";
+export const BASE_URL = "https://lenous-server.liara.run/api/v1";
+export const SOCKET_URL = "https://lenous-server.liara.run";
+// export const BASE_URL = "http://localhost:3000/api/v1";
+// export const SOCKET_URL = "http://localhost:3000";
