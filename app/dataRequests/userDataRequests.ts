f3a1d8629183083ed3b4c4fbdae3de724216c366
Hamid Roohi <hamidroohi71@gmail.com>
2025-04-13 15:15:16 +0330

    commit to depoly on vercel

--- app/dataRequests/userDataRequests.ts
+++ app/dataRequests/userDataRequests.ts
@@ -4,7 +4,7 @@ import { BASE_URL } from "./constants";
 export const getUserCredit = async (address: string) => {
   return axios
     .get(`http://195.248.240.173:8000/api/user/balance/${address}`)
-    .then((res) => {
+    .then((res: any) => {
       return res;
     });
 };
